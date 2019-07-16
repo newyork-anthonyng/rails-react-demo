7fb19dfacc685cd59b1e14d79e2e2213b50392d7
Anthony Ng <newyork.anthonyng@gmail.com>
2019-07-16 16:17:58 -0400

    Add App component

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static#index'
-
   namespace :v1, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
   end
+
+  # Forward non-Ajax requests to StaticController#index
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html? 
+  end
+
+  root 'static#index'
 end
