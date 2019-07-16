02e9885b5d95b4287d851587b8b894411a19b6f0
Anthony Ng <newyork.anthonyng@gmail.com>
2019-07-16 16:23:58 -0400

    Refactor with react hooks

--- app/javascript/components/Counter.js
+++ app/javascript/components/Counter.js
@@ -1,30 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 
-class Counter extends React.Component {
-  constructor(props) {
-    super(props);
+function Counter() {
+  const [count, setCount] = useState(0);
 
-    this.state = {
-      count: 0
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-
-  render () {
-    return (
-      <div>
-        <h1>Count: {this.state.count}</h1>
-        <button onClick={this.handleClick}>+</button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
 }
 
 export default Counter
