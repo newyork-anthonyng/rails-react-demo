8b3c8708bd6d814cb48e08dc41bfc1e95d7a281a
Anthony Ng <newyork.anthonyng@gmail.com>
2019-07-17 11:36:59 -0400

    Add server-side rendering

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,18 +1,19 @@
 import React from "react"
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+// import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HelloWorld from "./HelloWorld";
 import Counter from "./Counter";
 
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={() => "Home!"} />
-          <Route path="/hello" render={() => <HelloWorld greeting="Friend" />} />
-          <Route path="/counter" render={() => <Counter />} />
-        </Switch>
-      </BrowserRouter>
+      <Counter />
+      // <BrowserRouter>
+      //   <Switch>
+      //     <Route exact path="/" render={() => "Home!"} />
+      //     <Route path="/hello" render={() => <HelloWorld greeting="Friend" />} />
+      //     <Route path="/counter" render={() => <Counter />} />
+      //   </Switch>
+      // </BrowserRouter>
     );
   }
 }
